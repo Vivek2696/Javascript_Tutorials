16c00f2e851de44a306e42b3cb2ae41e2921e49c
unknown <Vivek2696>
2020-08-22 13:24:58 -0400

    Adding new folder with basic programming

---
+++ Basic_Javascript/Variables.js
@@ -0,0 +1,20 @@
+var i = 5;
+var age = 23;
+var money = 50.05;
+
+var Name = "Viraj";
+name = 'Viraj';
+
+var isUserLoggedIn = true;
+
+// can change the type of the variable easily
+
+name = 50;
+
+
+// constant are used for fixed variables
+
+const title = 'My WebApp';
+
+/***Const value cannot be changed once assigned through out the code */
+// title = 'change title';

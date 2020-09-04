3e7710f8fb083ad9f2b65aea4862030085ae7fd9
Vivek D Patel <v0pate09@louisville.edu>
2020-09-03 22:57:16 -0400

    Variables practice

---
+++ Basic_Javascript/Operators.js
@@ -0,0 +1,17 @@
+//Assignment operators =
+var age = 27;
+var firstName = 'Vivek';
+var money = 50.05;
+
+//Arithmetic operators
+age = age + 1;
+var newAge = age + 1;
+age += 1;
+var fullName = firstName + ' Patel';
+
+//comparision operators
+var canAfford = money >= 20;
+
+//logical operators
+var cannotAfford = !canAfford;
+var canBuy = canAfford && age >= 18;
